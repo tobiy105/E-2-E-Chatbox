1a42ccc0f31a45d5695b558032021c92ac12a63f
tobiy105 <tobiy105@gmail.com>
2022-12-15 10:33:42 +0000

    Fixed searchbar & adding channels

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ChannelList, useChatContext } from 'stream-chat-react';
-import { ChannelSearch, TeamChannelList, TeamChannelPreview } from 'stream-chat-react';
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 import Cookies from 'universal-cookie';
 import webIcon from '../assets/index.png'
 import LogoutIcon from '../assets/logout.png'
@@ -36,6 +36,38 @@ const ChannelListContainer = () => {
         <div className='channel-list__list__wrapper'>
             <CompanyHeader />
             <ChannelSearch />
+            <ChannelList 
+                filters={{}}
+                channelRenderFilterFn={() => {}}
+                List={(listProps) => (
+                    <TeamChannelList 
+                        {...listProps}
+                        type="team"
+                    />
+                )}
+                Preview={(previewProps)  => (
+                    <TeamChannelPreview 
+                        {...previewProps}
+                        type="team"
+                    />
+                )}
+            />
+            <ChannelList 
+                filters={{}}
+                channelRenderFilterFn={() => {}}
+                List={(listProps) => (
+                    <TeamChannelList 
+                        {...listProps}
+                        type="messaging"
+                    />
+                )}
+                Preview={(previewProps)  => (
+                    <TeamChannelPreview 
+                        {...previewProps}
+                        type="messaging"
+                    />
+                )}
+            />
         </div>
     </>
   );
